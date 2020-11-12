f8c58408f40d3745f6aec9bb8616be23b2dfad81
El John <eljohn.bonga@g.msuiit.edu.ph>
2020-11-12 15:04:50 +0800

    changed full_name parameter to required

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -3,7 +3,7 @@ from flask_restx import Namespace, fields
 class UserDto:
   api = Namespace('user', description='user related operations')
   create_user = api.model('create_user', {
-    'full_name': fields.String(readonly=True, description='user fullname'),
+    'full_name': fields.String(required=True, description='user fullname'),
     'email': fields.String(required=True, description='user email'),
     'password': fields.String(required=True, description='user password'),
     'contact_number': fields.String(required=True, description='user contact number')

72cca55069aff0a190d015cc5a4e74124eca9c23
El John <eljohn.bonga@g.msuiit.edu.ph>
2021-06-20 14:37:02 +0800

    made some changes

--- app/main/config.py
+++ app/main/config.py
@@ -10,7 +10,8 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'development.db')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:051098@localhost:5432/foodhub_development_db'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:051098@localhost:5432/foodhub_development_db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://nnznxlqiiahjvl:16f6e383e355f2a41197d43f35f710bfaf6203405ed78558b4209fec4de1b04c@ec2-34-202-54-225.compute-1.amazonaws.com:5432/dc9gvv2q7q3cr5'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -24,7 +25,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgres://nnznxlqiiahjvl:16f6e383e355f2a41197d43f35f710bfaf6203405ed78558b4209fec4de1b04c@ec2-34-202-54-225.compute-1.amazonaws.com:5432/dc9gvv2q7q3cr5'
+    # SQLALCHEMY_DATABASE_URI = 'postgres://nnznxlqiiahjvl:16f6e383e355f2a41197d43f35f710bfaf6203405ed78558b4209fec4de1b04c@ec2-34-202-54-225.compute-1.amazonaws.com:5432/dc9gvv2q7q3cr5'
 
 config_by_name = dict(
     dev=DevelopmentConfig,
